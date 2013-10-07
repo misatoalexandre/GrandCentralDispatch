c1f867d69e8a32902c30d868d26ca914c8b5345c
misatoalexandre <misatotina.alexandre@gmail.com>
2013-10-07 17:53:28 -0400

    GCD: creating a new queue and run a long operation there to avoid freezing the UI.

--- GCD/ViewController.m
+++ GCD/ViewController.m
@@ -13,10 +13,16 @@
 @end
 
 @implementation ViewController
+ dispatch_queue_t myQueue;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+   // dispatch_queue_t myQue= dispatch_queue_create("myQueueName",NULL);
+   //  dispatch_async(myQue, ^{[];});
+    
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +32,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonPressed:(id)sender {
+    self.resutLabel.text=@"Please wait while we pick a number!";
+
+    //create a queue
+   myQueue=dispatch_queue_create("com.misato.gcd", NULL);
+    //run longRunningOperation in the new queue.
+    dispatch_async(myQueue, ^{[self longRunningOperation];});
+}
+
+-(void)longRunningOperation{
+    [NSThread sleepForTimeInterval:5];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{[self.resutLabel setText:[NSString stringWithFormat:@"Results: %d", arc4random()]];});
+    
+}
 @end
